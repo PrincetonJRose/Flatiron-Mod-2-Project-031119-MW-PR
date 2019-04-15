575944c318b77e0d3262b03bacbab770dc85d88f
PrincetonJRose <decoyzpr@gmail.com>
2019-04-15 13:16:01 -0400

    Added super basic login functionality.

--- pride-guide/app/controllers/application_controller.rb
+++ pride-guide/app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+    helper_method :current_user
+
+    def current_user
+        user_id = session[:user_id]
+        if user_id.present?
+            User.find(user_id)
+        end
+    end
+
+    def authorize!
+        unless current_user
+            @errors = "Login is required to view content"
+            redirect_to login_path
+        end
+    end
+
 end
--- pride-guide/config/routes.rb
+++ pride-guide/config/routes.rb
@@ -2,16 +2,16 @@ Rails.application.routes.draw do
 
   resources :users, only: [:index, :show, :create, :edit, :update, :destroy]
   
-  get "/", to: "static_pages#home"
-  get "/about", to: "static_pages#about"
-  get "/contact", to: "static_pages#contact"
-  get "/help", to: "static_pages#help"
+  get "/", to: "static_pages#home", as: "home"
+  get "/about", to: "static_pages#about", as: "about"
+  get "/contact", to: "static_pages#contact", as: "contact"
+  get "/help", to: "static_pages#help", as: "help"
 
   get "/register", to: "users#new", as: "register"
 
-  get "/login", to: "sessions#new"
+  get "/login", to: "sessions#new", as: "login"
   post "/login", to: "sessions#create"
-  delete "/logout", to: "sessions#destroy"
+  delete "/logout", to: "sessions#destroy", as: "logout"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
