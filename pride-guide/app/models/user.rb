1fbf0a6c09d57b9c8b80fb5e2a7df164d870f80e
PrincetonJRose <decoyzpr@gmail.com>
2019-04-16 16:29:22 -0400

    Added new image files for current resources whilst adding in an image column to the resource table. Added Google maps link for each location to the resource show page. Whew... that took a while!  ^_^

--- pride-guide/app/models/user.rb
+++ pride-guide/app/models/user.rb
@@ -15,13 +15,15 @@ class User < ApplicationRecord
     has_many :likes
     has_many :comments
 
+    ## Associations for the friends class/table
     has_many :active_friends, class_name:  "Friend", foreign_key: "friender_id", dependent: :destroy
     has_many :passive_friends, class_name:  "Friend",
     foreign_key: "friend_id",
     dependent: :destroy
     has_many :friends, through: :active_friends, source: :friend
     has_many :frienders, through: :passive_friends, source: :friender
-    
+    ###########################################
+
     validates :username, length: { in: 6..16 }
     validates :username, presence: true
     validates :username, uniqueness: true
