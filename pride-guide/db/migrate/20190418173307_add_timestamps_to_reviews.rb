4e69db3f59a0f32b54b685a1127b00d4cc379250
PrincetonJRose <decoyzpr@gmail.com>
2019-04-18 16:45:35 -0400

    Finished home page. Made sure login was required to access most of the site. Great stuffgit add .!  ^_^

--- pride-guide/db/migrate/20190418173307_add_timestamps_to_reviews.rb
+++
@@ -1,9 +0,0 @@
-class AddTimestampsToReviews < ActiveRecord::Migration[5.2]
-  def change
-  add_timestamps :reviews, null: true 
-  long_ago = DateTime.new(2000, 1, 1)
-  Review.update_all(created_at: long_ago, updated_at: long_ago)
-  change_column_null :reviews, :created_at, false
-  change_column_null :reviews, :updated_at, false
-  end
-end
--- pride-guide/db/schema.rb
+++ pride-guide/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_18_173307) do
+ActiveRecord::Schema.define(version: 2019_04_17_184048) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
@@ -81,6 +81,8 @@ ActiveRecord::Schema.define(version: 2019_04_18_173307) do
     t.integer "user_id"
     t.integer "post_id"
     t.integer "review_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_likes_on_post_id"
     t.index ["review_id"], name: "index_likes_on_review_id"
     t.index ["user_id"], name: "index_likes_on_user_id"
@@ -96,6 +98,8 @@ ActiveRecord::Schema.define(version: 2019_04_18_173307) do
     t.integer "user_id"
     t.string "title"
     t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
